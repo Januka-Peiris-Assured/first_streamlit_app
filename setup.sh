9af3a1aa2d35c1d1de51259b2dc9694e80000690
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 14:51:14 +0000

    Create

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export SNOWFLAKE_USER=<your_user>
+export SNOWFLAKE_PASSWORD=<your_password>
+export SNOWFLAKE_ACCOUNT=<your_account>
+export SNOWFLAKE_WAREHOUSE=<your_warehouse>
+export SNOWFLAKE_DATABASE=<your_database>
+export SNOWFLAKE_SCHEMA=<your_schema>
