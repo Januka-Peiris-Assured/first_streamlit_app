81ee8dd027ce99d9845e05d1300a9415e1a8e3fa
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 15:41:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ selected_table = st.selectbox("Table Name", tables)
 
 # Retrieve table columns and preview data
 if selected_table:
-    cursor.execute(f"DESCRIBE TABLE {selected_table}")
+    cursor.execute(f"DESCRIBE TABLE {selected_database}."{selected_schema}"."{selected_table}"")
     columns = [row[0] for row in cursor.fetchall()]
 
     cursor.execute(f"SELECT * FROM {selected_table} LIMIT 10")
