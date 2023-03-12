b088187b3879cc687d74301cab3f0c58bf42dbab
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 15:57:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,69 @@
 import streamlit as st
-import psycopg2
-import pandas as pd
+import sqlite3
 
-# Database connection
-conn = psycopg2.connect(
-    host="localhost",
-    database="mydatabase",
-    user="myuser",
-    password="mypassword"
-)
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('test.db')
+        print(f'successful connection: {sqlite3.version}')
+    except sqlite3.Error as e:
+        print(e)
 
-# Function to fetch data from database
-def fetchData(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        rows = cur.fetchall()
-        return rows
+    return conn
 
-# Build Streamlit dataframe
-def buildTable(data):
-    headers = [x[0] for x in data.description]
-    rows = [list(row) for row in data]
-    return pd.DataFrame(rows, columns=headers)
+def execute_query(conn, query):
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        conn.commit()
+        print('Query executed successfully')
+    except sqlite3.Error as e:
+        print(f'Error executing query: {e}')
 
-# Query data from database
-query = "SELECT * FROM mytable"
-data = fetchData(query)
+def create_table(conn):
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS customers (
+            id INTEGER PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT,
+            email TEXT
+        );
+    '''
+    execute_query(conn, create_table_query)
 
-# Build Streamlit dataframe and display
-df = buildTable(data)
-st.dataframe(df)
+def insert_data(conn):
+    insert_data_query = '''
+        INSERT INTO customers (first_name, last_name, email)
+        VALUES
+            ('John', 'Doe', 'john.doe@email.com'),
+            ('Jane', 'Doe', 'jane.doe@email.com')
+    '''
+    execute_query(conn, insert_data_query)
+
+def select_all(conn):
+    select_all_query = '''
+        SELECT * FROM customers;
+    '''
+    cursor = conn.cursor()
+    cursor.execute(select_all_query)
+    rows = cursor.fetchall()
+    headers = [desc[0] for desc in cursor.description]
+    return rows, headers
+
+def main():
+    st.title('Test SQLite Database')
+
+    conn = create_connection()
+    if conn is not None:
+        create_table(conn)
+        insert_data(conn)
+        rows, headers = select_all(conn)
+
+        st.write('### Data in Streamlit DataFrame')
+        st.dataframe(rows, headers=headers)
+        
+    else:
+        st.write('Error creating database connection')
+
+if __name__ == '__main__':
+    main()
