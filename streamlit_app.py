3e9821b823d8bd76234a2cb8ca114b719a105c12
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 16:43:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,43 +7,48 @@ account = st.text_input("Snowflake Account Name")
 user = st.text_input("User Name")
 password = st.text_input("Password", type="password")
 
-# Connect to Snowflake
-conn = snowflake.connector.connect(
-    account=account,
-    user=user,
-    password=password
-)
+# Function to connect to Snowflake
+def connect_snowflake(account, user, password):
+    conn = snowflake.connector.connect(
+        account=account,
+        user=user,
+        password=password
+    )
+    return conn
 
-# Retrieve list of virtual warehouses, databases, and schemas
-cursor = conn.cursor()
-cursor.execute("SHOW WAREHOUSES")
-warehouses = [row[0] for row in cursor.fetchall()]
+# Connect to Snowflake when button is clicked
+if st.button("Login"):
+    conn = connect_snowflake(account, user, password)
 
-# Select Snowflake role
-cursor.execute("SELECT CURRENT_ROLE()")
-current_role = cursor.fetchone()[0]
-cursor.execute("SHOW ROLES")
-roles = [row[1] for row in cursor.fetchall()]
+    # Retrieve list of virtual warehouses, databases, and schemas
+    cursor = conn.cursor()
+    cursor.execute("SHOW WAREHOUSES")
+    warehouses = [row[0] for row in cursor.fetchall()]
 
-selected_warehouse = st.selectbox("Virtual Warehouse", warehouses)
-selected_role = st.selectbox("Role", roles, index=roles.index(current_role))
+    # Select Snowflake role
+    cursor.execute("SELECT CURRENT_ROLE()")
+    current_role = cursor.fetchone()[0]
+    cursor.execute("SHOW ROLES")
+    roles = [row[1] for row in cursor.fetchall()]
 
-cursor.execute("USE ROLE {}".format(selected_role))
+    selected_warehouse = st.selectbox("Virtual Warehouse", warehouses)
+    selected_role = st.selectbox("Role", roles, index=roles.index(current_role))
 
-cursor.execute("SHOW DATABASES")
-databases = [row[1] for row in cursor.fetchall()]
+    cursor.execute("USE ROLE {}".format(selected_role))
 
-cursor.execute("SHOW SCHEMAS")
-schemas = [row[1] for row in cursor.fetchall()]
+    cursor.execute("SHOW DATABASES")
+    databases = [row[1] for row in cursor.fetchall()]
 
-# Display options for virtual warehouse, database, schema, and table
-selected_database = st.selectbox("Database", databases)
-selected_schema = st.selectbox("Schema", schemas)
-cursor.execute(f"SHOW TABLES IN {selected_database}.{selected_schema}")
-tables = [row[1] for row in cursor.fetchall()]
-selected_table = st.selectbox("Table Name", tables)
+    cursor.execute("SHOW SCHEMAS")
+    schemas = [row[1] for row in cursor.fetchall()]
 
-if st.button("Load Table"):
+    # Display options for virtual warehouse, database, schema, and table
+    selected_database = st.selectbox("Database", databases)
+    selected_schema = st.selectbox("Schema", schemas)
+    cursor.execute(f"SHOW TABLES IN {selected_database}.{selected_schema}")
+    tables = [row[1] for row in cursor.fetchall()]
+    selected_table = st.selectbox("Table Name", tables)
+
     # Retrieve table columns and preview data
     def load_data():
         cursor.execute(f"SELECT * FROM {selected_database}.{selected_schema}.{selected_table} LIMIT 10")
@@ -80,3 +85,7 @@ if st.button("Load Table"):
             # Add new rows
             new_rows = edited_df.loc[edited_df["_st_state"].isin(["new", "new_row"]), :]
             if not new_rows.empty:
+                new_rows.drop(columns=["_st_state"], inplace=True)
+                new_rows.to_sql(name=selected_table, con=conn, schema=selected_schema, index=False, if_exists="append")
+
+            st.write("Changes saved!")
