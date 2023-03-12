07af4c3e078e3fa4dcae52800b948587ae4b3251
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 16:23:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,87 +1,79 @@
-import snowflake.connector
 import streamlit as st
 import pandas as pd
+import snowflake.connector
+from snowflake.connector import Error
+from sqlalchemy import create_engine
+import urllib.parse
 
-# Get Snowflake account details from user input
-account = st.text_input("Snowflake Account Name")
-user = st.text_input("User Name")
-password = st.text_input("Password", type="password")
+st.set_page_config(page_title="Snowflake App")
+
+# SNOWFLAKE CREDENTIALS
+user = st.secrets["user"]
+password = st.secrets["password"]
+account = st.secrets["account"]
+warehouse = st.secrets["warehouse"]
+database = st.secrets["database"]
+schema = st.secrets["schema"]
+
+params = urllib.parse.quote_plus(
+    f"account={account}&user={user}&password={password}&warehouse={warehouse}"
+)
+engine = create_engine(f"snowflake://{params}/DWH", pool_size=5)
 
 # Connect to Snowflake
-conn = snowflake.connector.connect(
-    account=account,
-    user=user,
-    password=password
-)
+try:
+    with snowflake.connector.connect(
+        user=user,
+        password=password,
+        account=account,
+        warehouse=warehouse,
+        database=database,
+        schema=schema,
+    ) as conn:
+        st.sidebar.write(f"Connected to Snowflake: `{database}`.`{schema}`")
+        # control tables
+        dim_managers = pd.read_sql_query(
+            f"SELECT * FROM DWH.CONTROL_TABLES.DIM_MANAGERS", conn
+        )
+        st.write(dim_managers)
 
-# Retrieve list of virtual warehouses, databases, and schemas
-cursor = conn.cursor()
-cursor.execute("SHOW WAREHOUSES")
-warehouses = [row[0] for row in cursor.fetchall()]
+        # Create a DataFrame
+        data = {"name": ["John", "Paul", "George", "Ringo"], "age": [80, 78, 78, 81]}
+        df = pd.DataFrame(data)
 
-# Select Snowflake role
-cursor.execute("SELECT CURRENT_ROLE()")
-current_role = cursor.fetchone()[0]
-cursor.execute("SHOW ROLES")
-roles = [row[1] for row in cursor.fetchall()]
+        # Display the DataFrame
+        st.write(df)
 
-selected_warehouse = st.selectbox("Virtual Warehouse", warehouses)
-selected_role = st.selectbox("Role", roles, index=roles.index(current_role))
+        # Allow the user to edit the DataFrame
+        st.write("Edit DataFrame")
+        st.dataframe(df, editable=True)
 
-cursor.execute("USE ROLE {}".format(selected_role))
+        # Get the edited DataFrame
+        edited_df = st.session_state.df
+        new_table_name = st.text_input("Enter a new table name", "new_table")
 
-cursor.execute("SHOW DATABASES")
-databases = [row[1] for row in cursor.fetchall()]
-
-cursor.execute("SHOW SCHEMAS")
-schemas = [row[1] for row in cursor.fetchall()]
-
-# Display options for virtual warehouse, database, schema, and table
-selected_database = st.selectbox("Database", databases)
-selected_schema = st.selectbox("Schema", schemas)
-cursor.execute(f"SHOW TABLES IN {selected_database}.{selected_schema}")
-tables = [row[1] for row in cursor.fetchall()]
-selected_table = st.selectbox("Table Name", tables)
-
-# Retrieve table columns and preview data
-def load_data():
-    cursor.execute(f"SELECT * FROM {selected_database}.{selected_schema}.{selected_table} LIMIT 10")
-    data = cursor.fetchall()
-    df = pd.DataFrame(data, columns=[desc[0] for desc in cursor.description])
-    return df
-
-if selected_table:
-
-    df = load_data()
-    st.write(f"Columns: {', '.join(df.columns)}")
-    st.write("Data Preview:")
-    st.dataframe(df)
-
-    
-    st.write("Edit table")
-    edited_df = st.experimental_data_editor(df, num_rows="dynamic") # 👈 An editable dataframe
-    
-    # Write back changes to Snowflake table
-    # Save changes to Snowflake
-if st.button("Save Changes"):
-    cursor.execute(f"SELECT * FROM {selected_database}.{selected_schema}.{selected_table}")
-    data = cursor.fetchall()
-    original_df = pd.DataFrame(data, columns=[desc[0] for desc in cursor.description])
-    edited_df = edited_df.astype(str)  # Convert all columns to str
-
-    # Update existing rows
-    for i, row in edited_df.iterrows():
-        original_row = original_df.iloc[i]
-        if not row.equals(original_row):
-            set_values = ", ".join([f"{col} = '{row[col]}'" for col in row.index])
-            where_clause = " AND ".join([f"{col} = '{original_row[col]}'" for col in row.index])
-            update_query = f"UPDATE {selected_database}.{selected_schema}.{selected_table} SET {set_values} WHERE {where_clause}"
-            cursor.execute(update_query)
-
-    # Add new rows
-    new_rows = edited_df.loc[edited_df["_st_state"].isin(["new", "new_row"]), :]
-    if not new_rows.empty:
-        new_rows.drop(columns=["_st_state"], inplace=True)
-        new_rows.to_sql(name=selected_table, con=conn, schema=selected_schema, index=False, if_exists="append")
-
-    st.write("Changes saved!")
+        # Save changes to Snowflake table
+        if st.button("Save changes"):
+            # Update the "new" and "new_row" values to "changed"
+            edited_df.loc[edited_df["_st_state"].isin(["new", "new_row"]), "_st_state"] = "changed"
+            # Insert new rows
+            new_rows = edited_df.loc[edited_df["_st_state"].isin(["new", "new_row"]), :]
+            new_rows.to_sql(
+                name=new_table_name,
+                con=engine,
+                schema=schema,
+                index=False,
+                if_exists="append",
+            )
+            # Update changed rows
+            changed_rows = edited_df.loc[edited_df["_st_state"] == "changed", :]
+            changed_rows.to_sql(
+                name=new_table_name,
+                con=engine,
+                schema=schema,
+                index=False,
+                if_exists="append",
+            )
+            st.success("Changes saved to Snowflake!")
+except Error as e:
+    st.sidebar.error(f"Snowflake error occurred: {e}")
