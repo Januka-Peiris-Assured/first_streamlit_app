9f74f6ef9ed178475c1703dd1f23c2918596b2ec
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 14:56:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,77 +1,53 @@
-import streamlit as st
-import snowflake.connector as sf
+# Set default values
+database = ""
+schema = ""
 
-def connect_to_snowflake(user, password, account, warehouse):
-    conn = sf.connect(
-        user=user,
-        password=password,
-        account=account,
-        warehouse=warehouse,
-    )
+# Connect to Snowflake
+if st.sidebar.button("Connect"):
+    try:
+        cursor = connect_to_snowflake(user, password, account, warehouse)
+        st.success("Connected to Snowflake!")
+    except Exception as e:
+        st.error("Error connecting to Snowflake: " + str(e))
+        return
 
-    return conn.cursor()
+    # Select database and schema
+    cursor.execute("SHOW DATABASES")
+    databases = [row[0] for row in cursor.fetchall()]
+    database = st.sidebar.selectbox("Select a database", databases)
 
-def main():
-    st.sidebar.title("Snowflake Admin Tool")
+    cursor.execute("USE DATABASE " + database)
 
-    # Get Snowflake credentials from user
-    st.sidebar.header("Snowflake Credentials")
-    user = st.sidebar.text_input("User")
-    password = st.sidebar.text_input("Password", type="password")
-    account = st.sidebar.text_input("Account")
-    warehouse = st.sidebar.text_input("Warehouse")
+    cursor.execute("SHOW SCHEMAS")
+    schemas = [row[0] for row in cursor.fetchall()]
+    schema = st.sidebar.selectbox("Select a schema", schemas)
 
-    # Connect to Snowflake
-    if st.sidebar.button("Connect"):
-        try:
-            cursor = connect_to_snowflake(user, password, account, warehouse)
-            st.success("Connected to Snowflake!")
-        except Exception as e:
-            st.error("Error connecting to Snowflake: " + str(e))
-            return
+    cursor.execute("USE SCHEMA " + schema)
 
-        # Select database and schema
-        st.sidebar.header("Snowflake Databases")
-        cursor.execute("SHOW DATABASES")
-        databases = [row[0] for row in cursor.fetchall()]
-        database = st.sidebar.selectbox("Select a database", databases)
+    # Show table information
+    cursor.execute('SELECT * FROM information_schema.tables')
+    tables = cursor.fetchall()
+    st.write('### Tables in Snowflake:')
+    for table in tables:
+        st.write('- ' + table.table_name)
 
-        cursor.execute("USE DATABASE " + database)
+    # Show user information
+    cursor.execute('SHOW USERS')
+    users = cursor.fetchall()
+    st.write('### Users in Snowflake:')
+    for user in users:
+        st.write('- ' + user[0])
 
-        st.sidebar.header("Snowflake Schemas")
-        cursor.execute("SHOW SCHEMAS")
-        schemas = [row[0] for row in cursor.fetchall()]
-        schema = st.sidebar.selectbox("Select a schema", schemas)
+    # Show role information
+    cursor.execute('SHOW ROLES')
+    roles = cursor.fetchall()
+    st.write('### Roles in Snowflake:')
+    for role in roles:
+        st.write('- ' + role[0])
 
-        cursor.execute("USE SCHEMA " + schema)
-
-        # Show table information
-        cursor.execute('SELECT * FROM information_schema.tables')
-        tables = cursor.fetchall()
-        st.write('### Tables in Snowflake:')
-        for table in tables:
-            st.write('- ' + table.table_name)
-
-        # Show user information
-        cursor.execute('SHOW USERS')
-        users = cursor.fetchall()
-        st.write('### Users in Snowflake:')
-        for user in users:
-            st.write('- ' + user[0])
-
-        # Show role information
-        cursor.execute('SHOW ROLES')
-        roles = cursor.fetchall()
-        st.write('### Roles in Snowflake:')
-        for role in roles:
-            st.write('- ' + role[0])
-
-        # Show warehouse information
-        cursor.execute('SHOW WAREHOUSES')
-        warehouses = cursor.fetchall()
-        st.write('### Warehouses in Snowflake:')
-        for warehouse in warehouses:
-            st.write('- ' + warehouse[0])
-
-if __name__ == '__main__':
-    main()
+    # Show warehouse information
+    cursor.execute('SHOW WAREHOUSES')
+    warehouses = cursor.fetchall()
+    st.write('### Warehouses in Snowflake:')
+    for warehouse in warehouses:
+        st.write('- ' + warehouse[0])
