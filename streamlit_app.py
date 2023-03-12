453365611cd48dee2a69a49ff810c8d04454d78f
Januka-Peiris-Assured <107410117+Januka-Peiris-Assured@users.noreply.github.com>
2023-03-12 17:43:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,15 @@ account = st.text_input("Snowflake Account Name")
 user = st.text_input("User Name")
 password = st.text_input("Password", type="password")
 
-# Function to connect to Snowflake
-def connect_snowflake(account, user, password):
+# Connect to Snowflake
+conn = None
+
+if st.button("Login"):
     conn = snowflake.connector.connect(
         account=account,
         user=user,
         password=password
     )
-    return conn
-
-# Connect to Snowflake when button is clicked
-if st.button("Login"):
-    conn = connect_snowflake(account, user, password)
 
     # Retrieve list of virtual warehouses, databases, and schemas
     cursor = conn.cursor()
@@ -39,12 +36,11 @@ if st.button("Login"):
     cursor.execute("SHOW DATABASES")
     databases = [row[1] for row in cursor.fetchall()]
 
-    # Display options for virtual warehouse, database, schema, and table
-    selected_database = st.selectbox("Database", databases)
-
-    cursor.execute(f"SHOW SCHEMAS IN {selected_database}")
+    cursor.execute("SHOW SCHEMAS")
     schemas = [row[1] for row in cursor.fetchall()]
 
+    # Display options for virtual warehouse, database, schema, and table
+    selected_database = st.selectbox("Database", databases)
     selected_schema = st.selectbox("Schema", schemas)
     cursor.execute(f"SHOW TABLES IN {selected_database}.{selected_schema}")
     tables = [row[1] for row in cursor.fetchall()]
@@ -58,6 +54,7 @@ if st.button("Login"):
         return df
 
     if selected_table:
+
         df = load_data()
         st.write(f"Columns: {', '.join(df.columns)}")
         st.write("Data Preview:")
@@ -80,4 +77,13 @@ if st.button("Login"):
                 if not row.equals(original_row):
                     set_values = ", ".join([f"{col} = '{row[col]}'" for col in row.index])
                     where_clause = " AND ".join([f"{col} = '{original_row[col]}'" for col in row.index])
-                    update_query = f"UPDATE {selected_database}.{selected_schema}.{selected_table} SET
+                    update_query = f"UPDATE {selected_database}.{selected_schema}.{selected_table} SET {set_values} WHERE {where_clause}"
+                    cursor.execute(update_query)
+
+            # Add new rows
+            new_rows = edited_df.loc[edited_df["_st_state"].isin(["new", "new_row"]), :]
+            if not new_rows.empty:
+                new_rows.drop(columns=["_st_state"], inplace=True)
+                new_rows.to_sql(name=selected_table, con=conn, schema=selected_schema, index=False, if_exists="append")
+
+            st.write("Changes saved!")
